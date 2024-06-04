47aa444233b55117432758c3e8a6dbb1c44ac26e
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-04 14:25:15 +0200

    Rendered the `Outlet` as a component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-  return <h1>This is the Layout Route</h1>;
+  return (
+    <>
+      <h1>This is the Layout Route</h1>
+      <Outlet />
+    </>
+  );
 }
