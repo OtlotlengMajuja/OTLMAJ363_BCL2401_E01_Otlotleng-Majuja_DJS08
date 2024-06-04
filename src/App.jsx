22fa30691b5b04ff7e936329f89ef699e22896a2
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-04 14:17:17 +0200

    Imported my `Layout` function, and set the placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "/.pages/Vans";
 import VanDetail from "./pages/VanDetail";
+import Layout from "./components/Layout";
 
 import "./server";
 
@@ -10,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
