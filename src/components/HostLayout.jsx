72f13e86cc28be933ef2cce66fc6fd2faeb8a460
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-04 15:40:29 +0200

    Set the contents of my `HostLayout` function and added CSS styling

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HostLayout() {
-  return <h1>HostLayout</h1>;
+  return (
+    <nav className="host-nav">
+      <Link to="/host">Dashboard</Link>
+      <Link to="/host/income">Income</Link>
+      <Link to="/host/reviews">Reviews</Link>
+    </nav>
+  );
 }
