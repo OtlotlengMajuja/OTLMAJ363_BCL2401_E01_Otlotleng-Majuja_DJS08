209fdbfff33847f0478025f3a26b9041e4285383
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-04 11:28:44 +0200

    Set my state, then fetched the data from the server as soon as the Vans page loads

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Vans() {
+  const [vans, setVans] = React.useState([]);
+  React.useEffect(() => {
+    fetch("/api/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
   return <h1>Vans page goes here 🚐</h1>;
 }
