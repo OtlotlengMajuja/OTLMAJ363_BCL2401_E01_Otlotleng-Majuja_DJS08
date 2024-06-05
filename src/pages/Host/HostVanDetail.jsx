f97b1d99f23dcbd550b32eadd4f5c76387e1990b
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-05 09:58:00 +0200

    Set up the `useEffect` and the dependancy array

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -2,5 +2,8 @@ import React from "react";
 
 export default function HostVanDetail() {
   const [currentVan, setCurrentVan] = React.useState(null);
+
+  React.useEffect(() => {}, []);
+
   return <h1>Host Vans List Page</h1>;
 }
