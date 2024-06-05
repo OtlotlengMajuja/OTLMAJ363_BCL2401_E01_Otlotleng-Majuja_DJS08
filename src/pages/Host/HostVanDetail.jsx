e1fce651c3592231c20e1d89bdc3e21e02ad3774
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-05 09:55:19 +0200

    Initialized the state in the `HostVanDetail` component

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default function HostVans() {
+export default function HostVanDetail() {
+  const [currentVan, setCurrentVan] = React.useState(null);
   return <h1>Host Vans List Page</h1>;
 }
