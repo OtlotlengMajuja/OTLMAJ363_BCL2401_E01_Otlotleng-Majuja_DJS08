78893e2835320c4b39b0a748dd1bc02dd333f427
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-05 20:28:50 +0200

    Included the context for the `Outlet` function

--- src/pages/Host/HostVanInfo.jsx
+++ src/pages/Host/HostVanInfo.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
 
 export default function HostVanInfo() {
+  const { currentVan } = useOutletContext();
   return <h2>Detailed info goes here</h2>;
 }
--- src/pages/Host/HostVanPricing.jsx
+++ src/pages/Host/HostVanPricing.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
 
 export default function HostVanPricing() {
+  const { currentVan } = useOutletContext();
   return <h2>Pricing info goes here</h2>;
 }
