f5cf4b366910b0f78c64659f7990eaa017d33362
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-06 11:47:15 +0200

    Conditionally rendered the selected className for the 3 filter buttons

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -39,19 +39,25 @@ export default function Vans() {
       <div className="van-list-filter-buttons">
         <button
           onClick={() => setSearchParams({ type: "simple" })}
-          className="van-type simple"
+          className={`van-type simple ${
+            typeFilter === "simple" ? "selected" : ""
+          }`}
         >
           Simple
         </button>
         <button
           onClick={() => setSearchParams({ type: "luxury" })}
-          className="van-type luxury"
+          className={`van-type luxury ${
+            typeFilter === "luxury" ? "selected" : ""
+          }`}
         >
           Luxury
         </button>
         <button
           onClick={() => setSearchParams({ type: "rugged" })}
-          className="van-type rugged"
+          className={`van-type rugged ${
+            typeFilter === "rugged" ? "selected" : ""
+          }`}
         >
           Rugged
         </button>
