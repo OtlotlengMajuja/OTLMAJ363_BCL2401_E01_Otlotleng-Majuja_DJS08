79326f9f2f00b98e1f82ada8d07690909e5eefbe
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-06 10:49:22 +0200

    Logged `typeFilter`

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -6,6 +6,7 @@ export default function Vans() {
   const [vans, setVans] = React.useState([]);
 
   const typeFilter = searchParams.get("type");
+  console.log(typeFilter);
 
   React.useEffect(() => {
     fetch("/api/vans")
