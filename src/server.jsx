b6056604d59d5b1f0bd1eef677aebc6a4befaa46
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-06 22:13:24 +0200

    Updated the server file

--- src/server.jsx
+++ src/server.jsx
@@ -93,5 +93,24 @@ createServer({
       const id = request.params.id;
       return schema.vans.findBy({ id, hostId: "123" }).vans[0];
     });
+
+    this.post("/login", (schema, request) => {
+      const { email, password } = JSON.parse(request.requestBody);
+
+      const foundUser = schema.users.findBy({ email, password });
+      if (!foundUser) {
+        return new Response(
+          401,
+          {},
+          { message: "No user with those credentials found!" }
+        );
+      }
+
+      foundUser.password = undefined;
+      return {
+        user: foundUser,
+        token: "Enjoy your pizza, here's your tokens.",
+      };
+    });
   },
 });
