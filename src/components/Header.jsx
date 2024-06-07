75d48e465a9b78ff404765cdaf62ff5d4f135eae
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-07 09:48:28 +0200

    Included an icon for the login and a log out button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+  }
+
   return (
     <header>
       <Link className="site-logo" to="/">
@@ -26,6 +36,10 @@ export default function Header() {
         >
           Vans
         </NavLink>
+        <Link to="login" className="login-link">
+          <img src="../assets/images/avatar-icon.png" className="login-icon" />
+        </Link>
+        <button onClick={fakeLogOut}>X</button>
       </nav>
     </header>
   );
