631c304a6d57d56fea5d8927e805fac3eccc142d
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-07 09:35:45 +0200

    Called the navigate function to programmatically send the user somewhere on the website

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,9 @@ export default function Login() {
   });
   const [status, setStatus] = React.useState("idle");
   const [error, setError] = React.useState(null);
+
   const location = useLocation();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
